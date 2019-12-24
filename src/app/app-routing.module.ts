5d4e38682eecab8e86a6c2082dc30bbdc8d3cc9f
bougis <regis.bouchez@gmail.com>
2019-12-24 13:24:22 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AppComponent } from './app.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+      { path: '', redirectTo: 'liste', pathMatch: 'full' },
+      { path: 'liste', component: ProductListComponent },
+      { path: 'panier', component: CartDetailComponent },
+      { path: 'produits/:slug', component: ProductDetailComponent  },
+      { path: '**',  redirectTo: 'home', pathMatch: 'full'},
+    ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { OrderInfoComponent } from './order-info/order-info.component';
+import { ProductButtonsComponent } from './product-buttons/product-buttons.component';
+import { CartIconComponent } from './cart-icon/cart-icon.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { DataViewModule } from 'primeng/dataview';
+import { PanelModule } from 'primeng/panel';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    OrderInfoComponent,
+    ProductButtonsComponent,
+    CartIconComponent,
+    CartDetailComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DataViewModule,
+    PanelModule,
+    ProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
